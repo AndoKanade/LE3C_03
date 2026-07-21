433ce9c8082a8d2779142d2bc4162e6f8c0e8410
Kanade Andou <k024g0073@g.neec.ac.jp>
2026-07-21 10:53:44 +0900

    gamescene整理

--- Project/Game/scenes/GameScene.h
+++ Project/Game/scenes/GameScene.h
@@ -2,68 +2,40 @@
 
 #include "systems/BaseScene.h"
 #include "MyMath.h"
-#include "LevelManager.h"
 #include <memory>
 #include <string>
 #include <vector>
 
-// 前方宣言
 class Input;
 class Obj3D;
 class Obj3dCommon;
-class ParticleEmitter;
 class SpriteCommon;
 class Skybox;
 class SkyboxCommon;
 class Application;
-class Animation;
-class AnimationController;
 
 class GameScene : public BaseScene{
 public:
 	GameScene();
 	~GameScene() override;
 
-	// シーン管理
+	// シーンの初期化
 	void Initialize(Obj3dCommon* object3dCommon,Input* input,SpriteCommon* spriteCommon) override;
+	// シーンの終了処理
 	void Finalize() override;
+	// シーンの更新処理
 	void Update() override;
+	// シーンの描画処理
 	void Draw() override;
 
 private:
-	// 外部依存
+	// 外部から受け取るポインタ
 	Obj3dCommon* object3dCommon_ = nullptr;
 	Input* input_ = nullptr;
 	SpriteCommon* spriteCommon_ = nullptr;
 	Application* app_ = nullptr;
 
-	// 3Dオブジェクト
-	std::shared_ptr<Obj3D> planeObj_;
-	std::shared_ptr<Obj3D> fenceObj_;
-	std::shared_ptr<Obj3D> sphereObj_;
-	std::shared_ptr<Obj3D> terrainObj_;
-	std::shared_ptr<Obj3D> simpleSkinObj_;
-	std::shared_ptr<Obj3D> animationCube_;
-	std::shared_ptr<Obj3D> humanObj_;
-
-	// パーティクル管理
-	std::unique_ptr<ParticleEmitter> ringEmitter_;
-	std::unique_ptr<ParticleEmitter> circleEmitter_;
-	std::unique_ptr<ParticleEmitter> cylinderEmitter_;
-	std::unique_ptr<ParticleEmitter> shockwaveEmitter_;
-
-	// 環境・エフェクト
+	// スカイボックス
 	std::unique_ptr<SkyboxCommon> skyboxCommon_;
 	std::unique_ptr<Skybox> skybox_;
-
-	// アニメーション
-	std::unique_ptr<Animation> animation_;
-	std::unique_ptr<AnimationController> animationController_;
-	float animationTime_ = 0.0f;
-
-	// 設定・状態
-	const std::string kBgmPath_ = "resource/You_and_Me.mp3";
-	bool isPaused_ = false;
-
-	std::vector<std::shared_ptr<Obj3D>> levelObjects_;
 };
